c0a94c4158478a91b566af2c00b31416acbff78b
Yamaguchi <yamaguchi@psychic-vr-lab.com>
2018-05-20 12:08:02 +0900

    もろもろコミット

---
+++ MoonDiving-Unity/Assets/_Parts/Circle Checker/PassCircleChecker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassCircleChecker : MonoBehaviour {
+
+    public ParticleSystem particle;
+    public AudioClip sound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        particle.Play();
+        GetComponent<AudioSource>().clip = sound;
+        GetComponent<AudioSource>().Play();
+    }
+
+
+}
